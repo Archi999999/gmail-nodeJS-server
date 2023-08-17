71fde5be63dc715f14c7d9dc2c6ae4d3e9102730
Archi99999 <archi999999@gmail.com>
2023-08-17 16:55:11 +0300

    fix cors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express()
-app.use(cors())
+app.use(cors({origin:'*'}))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
