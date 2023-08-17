69135e66a58a95bb3241c840ab1df6e8f3a089d6
Archi99999 <archi999999@gmail.com>
2023-08-17 11:28:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+let smtp_login = process.env.SMTP_LOGIN
+let smtp_password = process.env.SMTP_PASSWORD
+
+let transporter = nodemailer.createTransport({
+    // host: "smpt.ethereal.email",
+    service: "gmail",
+    // port: 25,
+    // requireTLS: true,
+    // secure: false,
+    auth: {
+        user: smtp_login,
+        pass: smtp_password
+    }
+    , tls: {
+        rejectUnauthorized: false
+    }
+})
+
+app.get('/', function (req, res) {
+    res.send('hello world123')
+});
+
+app.get('/rew', function (req, res) {
+    res.send('rew adwawdrew')
+});
+
+app.post('/sendMessage', async function (req, res) {
+    const {email, text, name} = req.body;
+    try {
+        const info = await transporter.sendMail({
+
+            from: 'GO TO JOB',
+            to: 'gilmanov.dev@gmail.com',
+            subject: 'тестовое сообщение',
+            // // text: 'hello world?',
+            html: `<b>сообщение с вашего портфолио</b>
+            <div>${name}</div>
+            <div>${email}</div>
+            <div>${text}</div>
+            `
+
+        });
+        console.log('Email sent:', info.response, req.body);
+        res.send('Email sent successfully!');
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).send('Failed to send email.');
+    }
+})
+
+let port = process.env.PORT || 3003
+
+app.listen(port, function () {
+    console.log("Example app listening on port 3003!")
+})
+
